50330b37995bbeb87bbabdd45997c4152ff2691c
GauthamKrM <90104607+GauthamKrM@users.noreply.github.com>
2021-10-22 19:46:39 +0530

    child.py

---
+++ weak2.py
@@ -0,0 +1,12 @@
+#sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+#last = sports[(len(sports)-3):]
+#print(last)
+#by = "You are"
+#az = "doing a great "
+#io = "job"
+#qy = "keep it up!"
+#sen = [by,az,io,qy]
+#senfinal = sen[:3]+[","]+sen[3:]
+#space = ' '
+#message = space.join(senfinal)
+#print(message)
